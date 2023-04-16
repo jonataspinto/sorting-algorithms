b3f5cb7fc0ab77a8a96d27f1dd2e1aa9cedc98cc
jonataspinto <jonatastdb_jp@hotmail.com>
2023-04-15 22:54:44 -0300

    test(selectionSort): write test todo

---
+++ src/selectionSort/selectionSort.test.ts
@@ -0,0 +1,15 @@
+const mockNumber = {
+  input: [2, 7, 8, 3, 6, 4, 5, 1],
+  expect: [1, 2, 3, 4, 5, 6, 7, 8],
+};
+
+const mockNames = {
+  input: ["bob", "peter", "mike", "ana"],
+  expect: [ 'ana', 'bob', 'mike', 'peter' ]
+};
+
+describe("Selection sort", () => {
+  it.todo("should sort a list of numbers correctly");
+
+  it.todo("should sort a list of names correctly");
+});
